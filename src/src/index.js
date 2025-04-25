371c8b693426f59b425d57492a367081974fea29
thangnguyengamo <thangmnguyen@gmail.com>
2025-04-25 19:41:51 +0700

    Update index.js

--- src/src/index.js
+++ src/src/index.js
@@ -3,20 +3,16 @@ export default {
     const ua = request.headers.get("User-Agent") || "";
     const url = new URL(request.url);
 
-    // Phân biệt thiết bị mobile hay desktop
     const device = /Mobile|Android|iPhone|iPad/i.test(ua) ? "mobile" : "desktop";
     url.searchParams.set("_cf_device", device);
 
-    // Tạo request mới với URL đã thay đổi
     const modifiedRequest = new Request(url.toString(), request);
     const cache = caches.default;
 
-    // Kiểm tra cache trước
     let response = await cache.match(modifiedRequest);
     if (!response) {
       response = await fetch(modifiedRequest);
 
-      // Xóa header Vary nếu có
       const newHeaders = new Headers(response.headers);
       newHeaders.delete("Vary");
 
@@ -25,8 +21,8 @@ export default {
         headers: newHeaders
       });
 
-      // Lưu lại cache
       ctx.waitUntil(cache.put(modifiedRequest, response.clone()));
+      ctx.waitUntil(cache.put(request, response.clone())); // cache cho URL gốc luôn
     }
 
     return response;
