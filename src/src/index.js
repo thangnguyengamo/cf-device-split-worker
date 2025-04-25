067412e85e84a20822800321ac7b24d8722cdcbf
thangnguyengamo <thangmnguyen@gmail.com>
2025-04-25 14:21:49 +0700

    Update index.js

--- src/src/index.js
+++ src/src/index.js
@@ -3,13 +3,31 @@ export default {
     const ua = request.headers.get("User-Agent") || "";
     const url = new URL(request.url);
 
-    if (/Mobile|Android|iPhone|iPad/i.test(ua)) {
-      url.searchParams.set("_cf_device", "mobile");
-    } else {
-      url.searchParams.set("_cf_device", "desktop");
+    // Gắn dấu hiệu phân biệt thiết bị
+    const device = /Mobile|Android|iPhone|iPad/i.test(ua) ? "mobile" : "desktop";
+    url.searchParams.set("_cf_device", device);
+
+    const modifiedRequest = new Request(url.toString(), request);
+
+    // Xử lý cache
+    const cache = caches.default;
+    const cacheKey = new Request(modifiedRequest.url, modifiedRequest);
+
+    let response = await cache.match(cacheKey);
+    if (!response) {
+      response = await fetch(modifiedRequest);
+
+      const newHeaders = new Headers(response.headers);
+      newHeaders.delete("Vary");
+
+      response = new Response(response.body, {
+        ...response,
+        headers: newHeaders
+      });
+
+      ctx.waitUntil(cache.put(cacheKey, response.clone()));
     }
 
-    const newRequest = new Request(url.toString(), request);
-    return fetch(newRequest);
+    return response;
   }
 }
